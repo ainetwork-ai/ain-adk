560af147263f4d91131cc1e9267108b608c43ca2
Yoojin Ko <db232100@gmail.com>
2025-06-16 16:08:49 +0900

    fix: comment out checking intent analyzer

--- src/ainagent.ts
+++ src/ainagent.ts
@@ -34,15 +34,17 @@ export class AINAgent {
       res.send('Welcome to AINAgent!');
     });
 
-    if (!this.intentAnalyzer) {
-      throw new Error('IntentAnalyzer is not set. Please set it before starting the server.');
-    }
+    // FIXME(yoojin): comment out until add intentAnalyzer
+    // if (!this.intentAnalyzer) {
+    //   throw new Error('IntentAnalyzer is not set. Please set it before starting the server.');
+    // }
+
     this.app.post('/query', async (req, res) => {
       const { model, message } = req.body;
 
       // TODO: Handle query type
       const intentPrompt = await this.intentAnalyzer?.handleQuery(req.body) || "";
-      const response = this.modelConns[model].chat(message);
+      const response = await this.modelConns[model].fetch(message, intentPrompt);
       res.json(response);
     });
 
