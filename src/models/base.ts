3f4b063ef522235d6fabcd03b2cb7cdab28ef06e
Yoojin Ko <db232100@gmail.com>
2025-06-19 13:32:53 +0900

    refactor: general tool

--- src/models/base.ts
+++ src/models/base.ts
@@ -1,9 +1,9 @@
-import { MCPTool } from "../intent/modules/mcp/tool.js";
+import { ExtTool } from "../intent/modules/mcp/tool.js";
 
 export abstract class BaseModel {
   constructor() {
   }
 
   abstract fetch(userMessage: string, intentPrompt?: string): Promise<any>;
-  abstract fetchWithContextMessage(messages: any[], tools?: MCPTool[]): Promise<any>;
+  abstract fetchWithContextMessage(messages: any[], tools?: ExtTool[]): Promise<any>;
 }
